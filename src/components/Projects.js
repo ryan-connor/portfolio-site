5b7c25fdd0f9d679485a68dfef53667b2a3a5e90
Connor <ryanconnor786@gmail.com>
2021-02-12 21:30:04 -0700

    Updated project display to render tiles based on available space

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import ProjectTile from './ProjectTile';
 import ExampleAppImage from './images/ExampleAppImage.png';
 import ticTacToe from './images/ticTacToe.png';
@@ -17,70 +17,70 @@ const Projects = (props) => {
     //render 6? projects based on array of project objects that can be passed in as, test number to render on screen
     let savedProjects = [
         {
-            title: 'Inventory App',
+            title: 'Inventory App0',
             desc: 'Simple MVC app that displays inventory information from a database and populates EJS templates for various routes.',
             image: ExampleAppImage,
             tags: ['Node','Express','MongoDB','EJS'],
             gitLink: '',
         },
         {
-            title: 'To Do List App',
+            title: 'To Do List App1',
             desc: 'To Do list app that allows users to create tasks with descriptions/due dates, group them under multiple task projects and update/delete them.',
             image: toDoList,
             tags: ['JavaScript','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'Mock Shopping Cart',
+            title: 'Mock Shopping Cart2',
             desc: 'Mock shopping website using React Router with various page routes and items that can be added to a shopping cart which then displays a page with all items/total cost',
             image: shoppingCart,
             tags: ['React','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'Weather App',
+            title: 'Weather App3',
             desc: 'Displays weather conditions based on user search criteria using openweather API.',
             image: weatherApp,
             tags: ['JavaScript','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'Tile Memory Game',
+            title: 'Tile Memory Game4',
             desc: 'Tile memory game that displays randomly mixed tiles that change position every time a user clicks and counts a high score.',
             image: tileGame,
             tags: ['React','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'Tic Tac Toe',
+            title: 'Tic Tac Toe5',
             desc: 'Tic-Tac-Toe game that allows 2 players to enter their names and then play tic-tac-toe against each other',
             image: ticTacToe,
             tags: ['JavaScript','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'Calculator',
+            title: 'Calculator6',
             desc: 'Simple calculator with all basic functionality (add,subtract,multiply,divide), decimals, and all clear buttons',
             image: calculator,
             tags: ['JavaScript','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'Etch-a-Sketch',
+            title: 'Etch-a-Sketch7',
             desc: 'Etch-a-Sketch/drawing type game that allows users to input a grid size and then displays a grid of squares that change colors upon hover and get 10% darker on each color change',
             image: etchASketch,
             tags: ['JavaScript','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'CV Generator',
+            title: 'CV Generator8',
             desc: 'CV generator app with template sections for each part of a typical resume that can be edited and added to the generated resume, and then updated/deleted',
             image: ExampleAppImage,
             tags: ['JavaScript','HTML','CSS'],
             gitLink: '',
         },
         {
-            title: 'Members Message Board',
+            title: 'Members Message Board9',
             desc: 'Message board CRUD app made with React frontend, an Express REST API Backend, and MongoDB. Users can create an account, and sign in to post messages. ',
             image: ExampleAppImage,
             tags: ['React', 'Express', 'MongoDB', 'REST API'],
@@ -95,51 +95,68 @@ const Projects = (props) => {
 
     let numProjects = savedProjects.length; 
 
-    let numToDisplay = 6;
+    const [numToDisplay, setNumToDisplay]= useState(6); //always sets initial state to 6 tiles
     const [start,setStart] = useState(0);
     const [end, setEnd] = useState(numToDisplay);
 
+    //code to dynamically render number of tiles to fit nicely in grid
+    useEffect(() => {
+        function resizeGrid() {
 
-    //function to cycle tiles forwards
-    const cycleProjects = () => {
-        if (numProjects-1 - end+1 >= numToDisplay){
-            console.log("space");
-            setStart(end);
-            setEnd(end+numToDisplay);
-        } else {
-            console.log("wrap");
-            console.log("new start", end);
-            console.log("new end", numToDisplay - (numProjects -1 -end));
-            setStart(end);
-            setEnd(numToDisplay-1 - (numProjects -1 -end));
+        let width = window.innerWidth;
+        let tileWidth= 390; //tile size to set for resize
+        let numTiles = Math.floor(width/tileWidth)*2;
+        console.log("start:", start);
+        console.log("end:", end);
+        console.log("should display:", numTiles);
+        
+        if (numTiles-numToDisplay !== 0) {
+            setNumToDisplay(numTiles);
+            setEnd(adjustIndex(end+(numTiles-numToDisplay)));
+        }
+    }
+        window.addEventListener('resize', resizeGrid)
+        return () => {
+          window.removeEventListener('resize', resizeGrid)
+    }
+    });
+
+    //function to adjust array indexes when go out of bounds
+    let adjustIndex = (num) => {
+        if (num>=0 && num<numProjects) {
+            //no adjustment needed
+            return num;
+        } else if (num<0) {
+            //index is negative
+            return numProjects + num;
+
+        } else if (num>= numProjects) {
+            //index is outside on high end
+            return num - numProjects;
         }
     };
 
-//function to cycle tiles backwards
-    const cycleProjectsBack = () => {
-        if (start >= numToDisplay){
-            console.log("space");
-            setStart(start-(numToDisplay));
-            setEnd(start);
-        } else {
-            console.log("wrap");
-            console.log("new start", end);
-            console.log("new end", numToDisplay - (numProjects -1 -end));
-            setStart(numProjects+1 - (numToDisplay- start +1));
-            setEnd(start);
-        }
+    let cycleProjectsForward = () => {
+        setStart(adjustIndex(start + numToDisplay));
+        setEnd(adjustIndex(end+numToDisplay));
+    };
+
+    let cycleProjectsBackward = () => {
+        setStart(adjustIndex(start- numToDisplay));
+        setEnd(adjustIndex(end-numToDisplay));
     };
 
 
     //function to cycle tiles
     let cycleTiles = () => {
-        if (start<end) {
+
+        //note that currently it displays no tiles when it gets too small to fit any
+        if (start<=end) {
             return tiles.slice(start,end);
         } else {
             return [...tiles.slice(start),...tiles.slice(0,end)];
         }
     };
-
 
 
     // let tilesToRender = tiles.slice(start,end);
@@ -152,9 +169,9 @@ const Projects = (props) => {
                     {tilesToRender}              
                 </div>
                 <div className="carousel">
-                    <div className="carouselControl" id="carouselLeft" onClick={cycleProjectsBack}>{'<'}</div>
+                    <div className="carouselControl" id="carouselLeft" onClick={cycleProjectsBackward}>{'<'}</div>
                     <div>View More Projects</div>
-                    <div className="carouselControl" id="carouselRight"onClick={cycleProjects}>{'>'}</div>
+                    <div className="carouselControl" id="carouselRight"onClick={cycleProjectsForward}>{'>'}</div>
                 </div>
 
             </div>
